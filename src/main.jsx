b1a225ea97cdd6a0dd35ec4a1ec16350579be164
JamiuKolawole04 <Jakko4real@gmail.com>
2023-06-16 09:30:22 +0100

    chore: adding home route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AOS from "aos";                             
-import "aos/dist/aos.css"; // You can also use <link> for styles
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 // ..
@@ -11,8 +11,16 @@ AOS.init({
   once: false,
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    index: true,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
